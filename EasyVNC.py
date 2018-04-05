2483cfa2e36cec5ccde6a29a62298265af2c931c
Lucas Coady <lcoady@uchicago.edu>
2018-04-05 15:37:13 -0500

    adding Qt5 instead of Tk

--- EasyVNC.py
+++ EasyVNC.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # local version number
-from version import VERSION
+import version
 
 import base64
 from binascii import hexlify
@@ -12,6 +12,7 @@ import socket
 import sys
 import time
 import traceback
+import io
 
 import socketserver
 import _thread as thread
@@ -19,13 +20,37 @@ import subprocess
 
 from binascii import hexlify
 
-from tkinter import *
-from tkinter.ttk import *
-from tkinter import messagebox as tkMessageBox
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 # non-standard libraries 
 import paramiko
 import d3des
+
+#this command will be executed on the remote host to start the Xvnc server session
+REMOTE_VNCCOMMAND = """
+	vncactive() {
+		 VNCACTIVE=($(ps -C Xvnc-core -o user,args | grep $USER | grep : | cut -d: -f2 | cut -d' ' -f1))
+	};
+	remove_locks() {
+		find /tmp/.X* -user $USER -exec rm '{}' \;
+	};
+	run_vnc() {
+		vncactive
+		case ${#VNCACTIVE[@]} in 
+			0)
+				#not running
+				remove_locks
+				/usr/bin/.remotex -geometry 1024x768 -randr 1680x1050,1280x800,2048x1150,1920x1200,1152x864,1600x900,1366x768,1280x1024,1440x900,2560x1440,1024x768,1920x1080 &>/dev/null
+				vncactive
+			;;
+		esac 
+
+		echo ${VNCACTIVE[0]}
+		echo 0
+	};
+	run_vnc
+	"""
 
 class Config():
 	def __init__(self):
@@ -114,56 +139,199 @@ def verbose(s):
 	#print(s, file=sys.stderr)
 	pass
 
+def excepthook(excType, excValue, tracebackobj):
+	"""
+	Global function to catch unhandled exceptions.
 
+	@param excType exception type
+	@param excValue exception value
+	@param tracebackobj traceback object
+	"""
 
-class MainUI(Frame):
-	"""Main Tkinter GUI"""
+	if str(excType) == """<class 'UserWarning'>""":
+		msg = str(excValue)
+	else:
+		notice = \
+			"""Hi. An unhandled exception was encountered. Please report the problem """\
+			"""by copying this dialog to <%s>.\n""" % \
+			("lucas@uchicago.edu")
+
+		build_info = \
+			"""Exception occured on version <%s> at %s.""" % (version.VERSION, time.strftime("%Y-%m-%d, %H:%M:%S"))
+
+		tbinfofile = io.StringIO()
+		traceback.print_tb(tracebackobj, None, tbinfofile)
+		tbinfofile.seek(0)
+		tbinfo = tbinfofile.read()
 	
-	def __init__(self, parent):
-		# initialize root frame and holders for values
-		Frame.__init__(self, parent)
-		self.remote_host = StringVar()
-		self.username = StringVar()
-		self.password = StringVar()
+		separator = '~' * 85
+
+		errmsg = '%s: \n%s' % (str(excType), str(excValue))
+
+		sections = [notice, separator, build_info, separator, tbinfo, errmsg]
+		msg = '\n'.join(sections)
+	
+	
+	errorbox = QMessageBox()
+	errorbox.setText(str(msg))
+	errorbox.exec_()
+
+class KQDialog(QDialog):
+	def centerWindow(self):
+		qr = self.frameGeometry()
+		cp = QDesktopWidget().availableGeometry().center()
+		qr.moveCenter(cp)
+		self.move(qr.topLeft())
+		
+	def closeEvent(self, event):
+		sys.exit()
+
+class LoginUI(KQDialog):
+	def __init__(self, parent=None):
+	
+		super().__init__()
+		
 		self.parent = parent
-
-		# fill in the forms with config data
 		
-		self.remote_host.set(config.remote_host)
-		self.username.set(config.username)
-
+		self.username = QLineEdit()
+		self.password = QLineEdit()
+		self.password.setEchoMode(QLineEdit.Password)
+		self.remote_host = QComboBox()
+		self.remote_host.addItems(config.hosts)
+		
+		self.resize(300,200)
+		self.centerWindow()
+		
+		# fill in the forms with config data	
+		if config.remote_host: self.remote_host.setCurrentIndex(config.hosts.index(config.remote_host))
+		self.username.setText(config.username)
+		
 		# describe the interface
-		self.parent.title("Connect to Server")
-		self.style = Style()
-		self.style.theme_use("default")
-
-		frame = Frame(self, relief='flat', borderwidth=10)
-
-		Label(frame, text='VNC Host:').grid(row=0, sticky=W)
-		hostOption = OptionMenu(frame, self.remote_host, "", *config.hosts)
-		hostOption.grid(row=0, column=1, pady=10, padx=5)
-
-		Label(frame, text='Username:').grid(row=1, sticky=W)
-		usernameEntry = Entry(frame, textvariable=self.username, width=16)
-		usernameEntry.grid(row=1, column=1, padx=5)
-
-		Label(frame, text='Password:').grid(row=2, sticky=W)
-		passwordEntry = Entry(frame, textvariable=self.password, width=16, show='•')
-		passwordEntry.grid(row=2, column=1)
-		passwordEntry.bind('<Return>', connect)
-
-		frame.pack(fill=BOTH, expand=1)
-		self.pack(fill=BOTH, expand=1)
+		self.setWindowTitle('EasyVNC Connect to Server')
 		
-		revisionLabel = Label(self, text="r. " + str(VERSION))
-		revisionLabel.pack(side=LEFT, padx=5, pady=5)
-
-		closeButton = Button(self, text="Quit", command=master.quit)
-		closeButton.pack(side=RIGHT, padx=5, pady=5)
+		self.password.returnPressed.connect(self.connect)
 		
-		connectButton = Button(self, text="Connect", command=connect)
-		connectButton.pack(side=RIGHT)
- 
+		self.connectButton = QPushButton("Connect")
+		self.connectButton.clicked.connect(self.connect)
+		
+		self.exitButton = QPushButton("Exit")
+		self.exitButton.clicked.connect(sys.exit)
+
+		self.rowA = QHBoxLayout()
+		self.rowA.addWidget(QLabel("VNC Host:"))
+		self.rowA.addWidget(self.remote_host)
+
+		self.rowB = QHBoxLayout()
+		self.rowB.addWidget(QLabel("Username:"))
+		self.rowB.addWidget(self.username)
+		
+		self.rowC = QHBoxLayout()
+		self.rowC.addWidget(QLabel("Password:"))
+		self.rowC.addWidget(self.password)
+		
+		self.hbox = QHBoxLayout()
+		self.hbox.addWidget(QLabel("v. " + str(version.VERSION)))
+		self.hbox.addStretch(1)
+		self.hbox.addWidget(self.connectButton)
+		self.hbox.addWidget(self.exitButton)
+		
+		self.vbox = QVBoxLayout()
+		self.vbox.addLayout(self.rowA)
+		self.vbox.addLayout(self.rowB)
+		self.vbox.addLayout(self.rowC)
+		self.vbox.addStretch(1)
+		self.vbox.addLayout(self.hbox)
+		
+		self.setLayout(self.vbox)
+
+	def connect(self):
+
+		if self.password.text() == "" or self.username.text() == '' or str(self.remote_host.currentText) == '':
+			QMessageBox.warning(self, "Incomplete Form", "You must supply a server name, username, and password.")
+			return
+
+		# save the form data into the config
+		config.username = self.username.text()
+		config.password = self.password.text()
+		config.remote_host = str(self.remote_host.currentText())
+		config.save()
+
+		verbose("%s@%s:%s" % (config.username, config.get_fqdn_remote_host(), config.remote_port))
+
+
+
+		# attempt to connect TCP socket to remote host, catch exceptions and throw warnings if there are problems	
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.connect((config.get_fqdn_remote_host(), config.remote_port))
+		except Exception as e:
+			if e.errno == 11001:
+				QMessageBox.warning(self, "Connect Failed", "Could not connect because the remote_host (%s) would not resolve.\n  There may be a problem with your internet connection." % (remote_host))
+			else:
+				QMessageBox.warning(self, "Connect Failed", "SSH negotiation failed for %s: %s" % (config.get_fqdn_remote_host(), str(e)))
+			return
+
+
+		try:
+			# start a new paramiko transport over TCP socket
+			t = paramiko.Transport(sock)
+			t.start_client()
+
+			# authenticate 
+			t.auth_password(config.username, config.password)
+		
+			# a new channel on the paramiko transport layer, ssh can have lots of channels 
+			# may be interesting to add more channels, for remote shells and stuff
+			chan = t.open_session()
+			# fire up Xvnc on remote system and capture the output
+			chan.exec_command(REMOTE_VNCCOMMAND)
+			stdin = chan.makefile('wb')
+			stdout = chan.makefile('rb')
+			stderr = chan.makefile_stderr('rb')
+			# expecting an integer from the remote_vnccommand script, to tell us what port to use
+			vnc_port = int(stdout.readline()) + 5900
+			verbose('Discovered VNC on port %i' % (vnc_port))
+
+			# get a clue to tell us if we are compatible with the remote
+			# compare remote_verison to local and if the remote is newer throw a warning
+			remote_version = int(stdout.readline())
+			verbose('Remote version %i' %(remote_version))
+			if remote_version > version.VERSION:
+				QMessageBox.warning(self,"Update Available", "There is a newer version of EasyVNC available for download.  Please download and install a new copy from sw.src.uchicago.edu in order to ensure continued use.")
+
+			# we are done with this control channel
+			chan.close()
+
+			# create a thread to forward packets to server
+			thread.start_new_thread(forward_tunnel, (vnc_port, 'localhost', vnc_port, t, ))
+
+			# start up the local vnc client
+			vncprocess = subprocess.Popen(config.vnccommand, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			# pipe the vncconfig into the new vnc client process
+			vncconfig = config.get_vncconfig(vnc_port)
+			verbose('vncconfig: ' + vncconfig)		
+			vncprocess.stdin.write(str.encode(vncconfig))
+			vncprocess.stdin.close()
+
+			# block until vnc exits
+			vncprocess.wait()			
+			self.show()
+			
+			# minimize the GUI
+			#self.hide()
+		except ValueError:
+			QMessageBox.warning(self, "Server Error", "%s did not respond as expected to the command \n %s\nContact server support and report this error." % (config.remote_host, remote_vnccommand))
+			t.close()
+			return
+		except paramiko.AuthenticationException:
+			QMessageBox.warning(self, "Connect Failed", "Authentication failed :(  You provided an incorrect username or password for %s" % (config.remote_host))
+			t.close()
+			return
+		except Exception as e:
+			QMessageBox.warning(self, "Unhandled Exception", "An application error has occured.\n Traceback: %s Dir:%s CMD: %s" % (traceback.format_exc(), config.scriptdir, config.vnccommand))
+			app.quit()
+	
+
 def forward_tunnel(local_port, remote_host, remote_port, transport):
 	"""Start a tunnel using the supplied transport object"""
 
@@ -201,7 +369,7 @@ def forward_tunnel(local_port, remote_host, remote_port, transport):
 			chan.close()
 			self.request.close()
 			verbose('Tunnel closed ')
-			master.quit()
+			app.quit()
 
 	# Start the Threading TCP server using the above defined Handler. This is actually
 	# the process that will forward packets between our side and the server
@@ -210,113 +378,20 @@ def forward_tunnel(local_port, remote_host, remote_port, transport):
 	forwardserver.allow_reuse_address = True
 	forwardserver.serve_forever()
 
-def connect(event=None):
-	if app.password.get() == "" or app.username.get() == '' or app.remote_host.get() == '':
-		tkMessageBox.showwarning("Incomplete Form", "You must supply a server name, username, and password.")
-		return
-
-	# save the form data into the config
-	config.username = app.username.get()
-	config.password = app.password.get()
-	config.remote_host = app.remote_host.get()
-	config.save()
-
-	verbose("%s@%s:%s" % (config.username, config.get_fqdn_remote_host(), config.remote_port))
-
-	# this command will be executed on the remote host to start the Xvnc server session
-	remote_vnccommand = """
-vncactive() {
-	 VNCACTIVE=($(ps -C Xvnc-core -o user,args | grep $USER | grep : | cut -d: -f2 | cut -d' ' -f1))
-};
-remove_locks() {
-	find /tmp/.X* -user $USER -exec rm '{}' \;
-};
-run_vnc() {
-	vncactive
-	case ${#VNCACTIVE[@]} in 
-		0)
-			#not running
-			remove_locks
-			/usr/bin/.remotex -geometry 1024x768 -randr 1680x1050,1280x800,2048x1150,1920x1200,1152x864,1600x900,1366x768,1280x1024,1440x900,2560x1440,1024x768,1920x1080 &>/dev/null
-			vncactive
-		;;
-	esac 
-
-	echo ${VNCACTIVE[0]}
-	echo 0
-};
-run_vnc
-"""
-
-	# attempt to connect TCP socket to remote host, catch exceptions and throw warnings if there are problems	
-	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect((config.get_fqdn_remote_host(), config.remote_port))
-	except Exception as e:
-		if e.errno == 11001:
-			tkMessageBox.showwarning("Connect Failed", "Could not connect because the remote_host (%s) would not resolve.\n  There may be a problem with your internet connection." % (remote_host))
-		else:
-			tkMessageBox.showwarning("Connect Failed", "SSH negotiation failed for %s: %s" % (config.get_fqdn_remote_host(), str(e)))
-		return
-
-
-	try:
-		# start a new paramiko transport over TCP socket
-		t = paramiko.Transport(sock)
-		t.start_client()
-
-		# authenticate 
-		t.auth_password(config.username, config.password)
+class MainWindow(QMainWindow):
+	def __init__(self):
+		QMainWindow.__init__(self)
 		
-		# a new channel on the paramiko transport layer, ssh can have lots of channels 
-		# may be interesting to add more channels, for remote shells and stuff
-		chan = t.open_session()
-		# fire up Xvnc on remote system and capture the output
-		chan.exec_command(remote_vnccommand)
-		stdin = chan.makefile('wb')
-		stdout = chan.makefile('rb')
-		stderr = chan.makefile_stderr('rb')
-		# expecting an integer from the remote_vnccommand script, to tell us what port to use
-		vnc_port = int(stdout.readline()) + 5900
-		verbose('Discovered VNC on port %i' % (vnc_port))
-
-		# get a clue to tell us if we are compatible with the remote
-		# compare remote_verison to local and if the remote is newer throw a warning
-		remote_version = int(stdout.readline())
-		verbose('Remote version %i' %(remote_version))
-		if remote_version > VERSION:
-			tkMessageBox.showwarning("Update Available", "There is a newer version of EasyVNC available for download.  Please download and install a new copy from sw.src.uchicago.edu in order to ensure continued use.")
-
-		# we are done with this control channel
-		chan.close()
-
-		# create a thread to forward packets to server
-		thread.start_new_thread(forward_tunnel, (vnc_port, 'localhost', vnc_port, t, ))
-
-		# start up the local vnc client
-		vncprocess = subprocess.Popen(config.vnccommand, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		# pipe the vncconfig into the new vnc client process
-		vncconfig = config.get_vncconfig(vnc_port)
-		verbose('vncconfig: ' + vncconfig)		
-		vncprocess.stdin.write(str.encode(vncconfig)  )
-		vncprocess.stdin.close()
+		self.setWindowTitle("EasyVNC")
 		
-		# minimize the GUI
-		master.withdraw()
-	except ValueError:
-			tkMessageBox.showwarning("Server Error", "%s did not respond as expected to the command \n %s\nContact server support and report this error." % (config.remote_host, remote_vnccommand))
-			t.close()
-			return
-	except paramiko.AuthenticationException:
-			tkMessageBox.showwarning("Connect Failed", "Authentication failed :(  You provided an incorrect username or password for %s" % (config.remote_host))
-			t.close()
-			return
-	except Exception as e:
-		tkMessageBox.showwarning("Unhandled Exception", "An application error has occured.\n Traceback: %s Dir:%s CMD: %s" % (traceback.format_exc(), config.scriptdir, config.vnccommand))
-		sys.exit(1)
 
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	mainwindow = MainWindow()
 
-master = Tk()
-master.geometry("300x200+300+300")
-app = MainUI(master)
-master.mainloop()  
+	loginui = LoginUI()
+	loginui.exec_()
+
+	mainwindow.show()
+
+	sys.exit(app.exec_())
